59fcb506b3b2dd000b6b4ea4531db237cf5f28e6
Aleksander V <o.voloskivets@gmail.com>
2023-06-08 22:12:31 +0300

    load file

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './scss/all.scss'
+// import MainComponent from './components/MainComponent'
+//
+// const component = MainComponent('My component text')
+//
+// document.querySelector('body').appendChild(component)
+
---
+++ webpack.config.prod.js
@@ -0,0 +1,37 @@
+const webpack = require('webpack')
+const miniCss = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/main.js',
+    output: {
+        path: __dirname + '/public',
+        filename: 'app.js',
+        assetModuleFilename: 'assets/images/[name]-[hash][ext]'
+    },
+    devServer: {
+        port: 5000,
+        static: './public',
+        hot: true
+    },
+    module: {
+        rules: [
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+          },
+            { test: /\.(s*)css$/, use: [
+              {loader: miniCss.loader},
+							{loader: 'css-loader', options: {url: false}},
+							{loader: 'sass-loader'},
+						] },
+         ]
+      },
+      plugins: [
+        new miniCss({
+           filename: 'app.css',
+        }),
+        new CopyPlugin({patterns: [{ from: 'src/libs/', to: 'libs/' }, { from: 'src/fonts/', to: 'fonts/' }, { from: 'src/images/', to: 'images/' }]}),
+     ]
+}
